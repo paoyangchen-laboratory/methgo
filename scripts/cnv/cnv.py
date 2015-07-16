744a9b1450048f09a890987eb6130d0e4d63daf9
Wen-Wei Liao <gattacaliao@gmail.com>
2015-07-16 22:53:30 +0800

    Let the succession parameter work

--- scripts/cnv/cnv.py
+++ scripts/cnv/cnv.py
@@ -84,16 +84,21 @@ def main():
     data = data[data['pval'] < args.pvalue]
     index = data.index.tolist()
     cnv_cand = []
+    temp = []
     prev = index[0]
     for next in index[1:]:
         if prev[0] == next[0]:
             if (next[1] - prev[1]) == args.winsize:
-                if len(cnv_cand) > 0:
-                    if cnv_cand[-1] != prev:
-                        cnv_cand.append(prev)
+                if len(temp) > 0:
+                    if temp[-1] != prev:
+                        temp.append(prev)
                 else:
-                    cnv_cand.append(prev)
-                cnv_cand.append(next)
+                    temp.append(prev)
+                temp.append(next)
+            else:
+                if len(temp) >= args.succession:
+                    cnv_cand.extend(temp)
+                temp = []
         prev = next
     data = data.ix[pd.MultiIndex.from_tuples(cnv_cand, names=['chr', 'start', 'end'])]
     root = os.path.splitext(os.path.basename(args.bamfile))[0]
@@ -120,11 +125,11 @@ def main():
         label.set_fontweight('bold')
     ax.tick_params(direction='out', length=6, width=2, labelsize='large', top='off', right='off', bottom='off')
     ax.set_xticks([(vlines[i] + vlines[i+1])/2 for i in xrange(len(vlines) - 1)])
-    ax.set_xticklabels([extract_chrnum(chr) for chr in chrs])
+    ax.set_xticklabels([extract_chrnum(chr) for chr in chrs], fontsize='large', fontweight='bold')
     #ax.set_xlabel('Chromosome', fontsize='xx-large', fontweight='bold')
-    ax.set_ylabel('Coverage (base)', fontsize='xx-large', fontweight='bold')
+    ax.set_ylabel('Coverage (base)', fontsize='large', fontweight='bold')
     fig.tight_layout()
-    plt.savefig('{}.cov.png'.format(root), dpi=300)
+    plt.savefig('{}.cnv.png'.format(root), dpi=300)
     plt.close(fig)
 
 if __name__ == '__main__':
