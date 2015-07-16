c8f77c7b58c437467eb979b1d381aeba34ca27d1
Wen-Wei Liao <gattacaliao@gmail.com>
2015-07-16 16:42:48 +0800

    Add the info of p-value

--- scripts/cnv/cnv.py
+++ scripts/cnv/cnv.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 from __future__ import division
 import os
+import math
+import copy
+import random
 import argparse
 import pysam
 import numpy as np
+import scipy.stats
+import pandas as pd
 import matplotlib.pyplot as plt
+
+def extract_chrnum(chr):
+    if chr.lower().startswith('chr'):
+        try:
+            return int(chr[3:])
+        except ValueError:
+            return chr[3:]
+    try:
+        return int(chr)
+    except ValueError:
+        return chr[3:]
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w', '--winsize', default=200000)
+    parser.add_argument('-w', '--winsize', type=int, default=200000)
+    parser.add_argument('-p', '--pvalue', type=float, default=0.05)
+    parser.add_argument('-s', '--succession', type=int, default=3)
     parser.add_argument('refindex')
     parser.add_argument('bamfile')
     args = parser.parse_args()
@@ -32,21 +50,56 @@ def main():
         gcov[chr][pos] = cov
 
     pos = 0
-    chrs = sorted(chrlen.keys())
+    chrs = sorted(chrlen.keys(), key=extract_chrnum)
     win_x = []
+    win_chr = []
     win_cov = []
     for chr in chrs:
         start = 0
         while (start + args.winsize) <= chrlen[chr]:
             win_x.append(pos+(args.winsize/2))
+            win_chr.append((chr, start, start+args.winsize))
             tmp = np.array(gcov[chr][start:start+args.winsize])
             tmp = tmp[~np.isnan(tmp)]
-            win_cov.append(np.mean(tmp))
+            #win_cov.append(np.mean(tmp))
+            win_cov.append(tmp.sum())
             start += args.winsize
             pos += args.winsize
 
-    plt.switch_backend('Agg')
+    shuffle_cov = []
+    sample_size = 100
+    for _ in xrange(sample_size):
+        temp = copy.copy(win_cov)
+        random.shuffle(temp)
+        shuffle_cov.append(temp)
+
+    chr_index = pd.MultiIndex.from_tuples(win_chr, names=['chr', 'start', 'end'])
+    shuffle_table = pd.DataFrame(zip(*shuffle_cov), index=chr_index)
+    data = pd.DataFrame(win_cov, index=chr_index, columns=['cov'], dtype=int)
+    data['mean'] = shuffle_table.mean(axis=1)
+    data['std'] = shuffle_table.std(axis=1)
+    data['zscore'] = data.apply(lambda s: (s['cov']-s['mean'])/(s['std']/math.sqrt(sample_size)), axis=1)
+    data['pval'] = data['zscore'].apply(scipy.stats.norm.sf)
+    data = data[['cov', 'pval']]
+    data = data[data['pval'] < args.pvalue]
+    index = data.index.tolist()
+    cnv_cand = []
+    prev = index[0]
+    for next in index[1:]:
+        if prev[0] == next[0]:
+            if (next[1] - prev[1]) == args.winsize:
+                if len(cnv_cand) > 0:
+                    if cnv_cand[-1] != prev:
+                        cnv_cand.append(prev)
+                else:
+                    cnv_cand.append(prev)
+                cnv_cand.append(next)
+        prev = next
+    data = data.ix[pd.MultiIndex.from_tuples(cnv_cand, names=['chr', 'start', 'end'])]
     root = os.path.splitext(os.path.basename(args.bamfile))[0]
+    data.to_csv('{}.cnv.txt'.format(root), sep='\t')
+
+    plt.switch_backend('Agg')
     vlines = [0]
     for i, chr in enumerate(chrs):
         vlines.append(vlines[i] + len(gcov[chr]))
@@ -67,9 +120,9 @@ def main():
         label.set_fontweight('bold')
     ax.tick_params(direction='out', length=6, width=2, labelsize='large', top='off', right='off', bottom='off')
     ax.set_xticks([(vlines[i] + vlines[i+1])/2 for i in xrange(len(vlines) - 1)])
-    ax.set_xticklabels(chrs)
+    ax.set_xticklabels([extract_chrnum(chr) for chr in chrs])
     #ax.set_xlabel('Chromosome', fontsize='xx-large', fontweight='bold')
-    ax.set_ylabel('Coverage', fontsize='xx-large', fontweight='bold')
+    ax.set_ylabel('Coverage (base)', fontsize='xx-large', fontweight='bold')
     fig.tight_layout()
     plt.savefig('{}.cov.png'.format(root), dpi=300)
     plt.close(fig)
